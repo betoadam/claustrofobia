9bcad18212a9e731a18cac43980396eb056c9b0a
Beto Adam <testes.atenas@gmail.com>
2019-03-28 14:58:46 -0300

    Commit Inicial

---
+++ Assets/Scripts/NewGame.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGame : MonoBehaviour
+{
+	public GameObject Cena1Barra;
+	public GameObject Cena1Start;
+	private bool cenaRV_flag = false;
+	private bool cenaCM_flag = false;
+	private bool cenaYT_flag = false;
+	public void LoadCena01(Transform wayPoint){
+		Cena1Barra.gameObject.SetActive(true);
+		Cena1Start.gameObject.SetActive(true);
+			
+	}
+	public void DeLoadCena01(Transform wayPoint){
+		cenaRV_flag = false;
+		cenaCM_flag = false;
+		cenaYT_flag = false;
+	}
+	public void StartCena01(Transform wayPoint){
+		cenaRV_flag = true;
+		StartCoroutine(cenaRV());
+	}
+	public void StartCenaCM(Transform wayPoint){
+		cenaCM_flag = true;
+		StartCoroutine(cenaCM());
+	}
+	public void StartCenaYT(Transform wayPoint){
+		cenaYT_flag = true;
+		StartCoroutine(cenaYT());
+	}
+	public void Menu(Transform wayPoint){
+		SceneManager.LoadScene("Menu");	
+	}
+	IEnumerator cenaRV(){
+        yield return new WaitForSeconds(5.0f);
+		if(cenaRV_flag)
+			SceneManager.LoadScene("RV");	
+    }
+	IEnumerator cenaCM(){
+        yield return new WaitForSeconds(5.0f);
+		if(cenaCM_flag)
+			SceneManager.LoadScene("VideoCamera");	
+    }
+	IEnumerator cenaYT(){
+        yield return new WaitForSeconds(5.0f);
+		if(cenaYT_flag)
+			SceneManager.LoadScene("VideoYT");	
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# claustrofobia
+
+Ambiente de simulação de lugares claustrofóbicos
